d8adaa32915e6933c591790690541fca630ef9c5
Areeb Ur Rehman <areebjutt026@gmail.com>
2024-05-22 23:52:37 +0500

    add

--- script.js
+++ script.js
@@ -6,11 +6,23 @@ let audioElement = new Audio('./assets/STUBBORN.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
 let gif = document.getElementById('gif');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 
 let songs = [
-    {songName: "Stubborn", filePath: "./assets/STUBBORN", coverPath: "covers/true love"},
+    {songName: "Stubborn", filePath: "./assets/STUBBORN", coverPath: "assets/1.stubborn"},
+    {songName: "Stubborn", filePath: "./assets/1.mp3", coverPath: "covers/1.cover"},
+    {songName: "Stubborn", filePath: "./assets/2.mp3", coverPath: "covers/2.cover"},
+    {songName: "Stubborn", filePath: "./assets/3.mp3", coverPath: "covers/3.cover"},
+    {songName: "Stubborn", filePath: "./assets/4.mp3", coverPath: "covers/4.cover"},
+    {songName: "Stubborn", filePath: "./assets/5.mp3", coverPath: "covers/5.cover"},
+    {songName: "Stubborn", filePath: "./assets/6.mp3", coverPath: "covers/6.cover"},
 ]
+
+// songs.forEach((element,i)=>{
+//     console.log(element, i);
+//     element.getElementsByClassName("img")[0].src = songs[i].filePath;
+// })
 
 // let audioElement = new Audio('1.mp3');
 // audioElement.play();
@@ -48,5 +60,26 @@ audioElement.addEventListener('timeupdate', ()=>{
 })
 
 myProgressBar.addEventListener('change', ()=>{
-    audioElement.currentTime = myProgressBar.value * audioElement. 
+    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
+
+Array.from(document.getElementsByClassName('songItemplay')).forEach((element)=>{
+    Element.addEventListener('click', (e)=>{
+        console.log(e);
+    })
+})
+
+const makeAllPlays = ()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.target.classList.add('fa-circle-pause');
+    })
+}
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click', (e)=>{
+        // console.log(e.target);
+        makeAllPlays();
+        e.target.classList.remove(' fa-circle-play');
+        e.target.classList.add(' fa-circle-pause');
+
+    })
 })
